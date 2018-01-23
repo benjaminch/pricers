59a04f2514f49920f8bba2a35d2827067d8dd26a
BenjaminCh <benjamin.chastanier@gmail.com>
2018-01-23 08:23:52 +0100

    Adding function comments

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -14,17 +14,32 @@ import (
 	"github.com/golang/glog"
 )
 
+// KeyDecodingMode : Describing how keys should be decoded.
 type KeyDecodingMode string
 
+// String : Returns the KeyDecodingMode string representation.
+//
+// Returns
+//    - string:    String representation of KeyDecodingMode object.
 func (kd KeyDecodingMode) String() string {
 	return string(kd)
 }
 
 const (
+	// Utf8 : Key should be decoded as utf-8 string.
 	Utf8 KeyDecodingMode = "utf-8"
+	// Hexa : Key should be decoded as hexa string.
 	Hexa KeyDecodingMode = "hexa"
 )
 
+// ParseKeyDecodingMode : Parses KeyDecodingMode from string.
+//
+// ParseKeyDecodingMode requires the following params:
+//    - input: 		        keydecodingmode string to parse.
+//
+// Returns
+//    - KeyDecodingMode:    KeyDecodingMode object representation of the input string.
+//    - error:              Error occuring while parsing the KeyDecodingMode or nil if no error.
 func ParseKeyDecodingMode(input string) (KeyDecodingMode, error) {
 	var err error
 	var parsed KeyDecodingMode
@@ -47,6 +62,16 @@ func ParseKeyDecodingMode(input string) (KeyDecodingMode, error) {
 	return parsed, err
 }
 
+// CreateHmac : Returns Hash from input string.
+//
+// CreateHmac requires the following params:
+//    - key: 				String key to transform.
+//    - isBase64: 			Is key is base 64?
+//    - KeyDecodingMode:    How should the key should be decoded?
+//
+// Returns
+//    - hash.Hash:          Key hash.
+//    - error:              Error occuring while creating the key or nil if no error.
 func CreateHmac(key string, isBase64 bool, mode KeyDecodingMode) (hash.Hash, error) {
 	var err error
 	var b64DecodedKey []byte
@@ -73,16 +98,31 @@ func CreateHmac(key string, isBase64 bool, mode KeyDecodingMode) (hash.Hash, err
 	return hmac.New(sha1.New, k), nil
 }
 
+// HmacSum : Returns Hmac sum bytes.
+//
+// HmacSum requires the following params:
+//    - hmac: 		    hmac hash.
+//    - buf: 			bytes buffer to write in the hmac.
+//
+// Returns
+//    - byte[]:         Hmac sum bytes.
 func HmacSum(hmac hash.Hash, buf []byte) []byte {
 	hmac.Reset()
 	hmac.Write(buf)
 	return hmac.Sum(nil)
 }
 
-func AddBase64Padding(encryptedPrice string) string {
+// AddBase64Padding : Returns base 64 string adding extra padding if needed.
+//
+// AddBase64Padding requires the following params:
+//    - base64Input: 	Base64 string to be padded.
+//
+// Returns
+//    - string:          Padded Base64 string.
+func AddBase64Padding(base64Input string) string {
 	var base64 string
 
-	base64 = encryptedPrice
+	base64 = base64Input
 
 	if i := len(base64) % 4; i != 0 {
 		base64 += strings.Repeat("=", 4-i)
@@ -91,6 +131,16 @@ func AddBase64Padding(encryptedPrice string) string {
 	return base64
 }
 
+// ApplyScaleFactor : Applies a scale factor to a given price.
+// Scaled price will be represented on 8 bytes.
+//
+// ApplyScaleFactor requires the following params:
+//    - price: 			Price to be scale.
+//    - scaleFactor: 	Factor to be applied to the price.
+//    - isDebugMode: 	Print debug logs.
+//
+// Returns
+//    - [8]byte:        Scaled priced on 8 bytes.
 func ApplyScaleFactor(price float64, scaleFactor float64, isDebugMode bool) [8]byte {
 	scaledPrice := [8]byte{}
 	binary.BigEndian.PutUint64(scaledPrice[:], uint64(price*scaleFactor))
--- tests_helpers/test_helpers.go
+++ tests_helpers/test_helpers.go
@@ -2,8 +2,16 @@ package testshelpers
 
 import "math"
 
+// FloatEquals : Returns two if two floats can be considered as equal.
+//
+// FloatEquals requires the following params:
+//    - a:  	First float to compare.
+//    - b:  	Second float to compare.
+//
+// Returns
+//    - bool:   True if both float are equal.
 func FloatEquals(a, b float64) bool {
-	var eps float64 = 0.00000001
+	var eps = 0.00000001
 	if math.Abs(a-b) < eps {
 		return true
 	}
